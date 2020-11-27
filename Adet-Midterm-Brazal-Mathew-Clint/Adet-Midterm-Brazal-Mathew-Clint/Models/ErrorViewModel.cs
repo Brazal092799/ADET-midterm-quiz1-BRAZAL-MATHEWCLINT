916b7b1bdcf0a523b6c2f189d6352e01e638cbc5
MATHEW CLINT BRAZAL <1815-18@itmlyceumalabang.onmicrosoft.com>
2020-11-28 00:42:21 +0800

    Commit a message

---
+++ Adet-Midterm-Brazal-Mathew-Clint/Adet-Midterm-Brazal-Mathew-Clint/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Adet_Midterm_Brazal_Mathew_Clint.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
